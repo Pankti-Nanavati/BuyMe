ae087e97fcc1a5ed7d729419398dd7c3e68f6b41
darshee-m <darshee.m@somaiya.edu>
2023-04-06 15:12:16 -0400

    Product API model

--- config/schema.sql
+++ config/schema.sql
@@ -3,11 +3,11 @@ CREATE DATABASE bm_auction_system;
 
 CREATE TABLE `user` (
   `email_id` varchar(45) NOT NULL,
-  `password` varchar(60) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `user_name` varchar(20) DEFAULT NULL,
+  `password` varchar(65) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
   `phone_number` tinytext,
-  `address` varchar(45) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`email_id`)
 );
 
@@ -25,6 +25,70 @@ INSERT INTO `bm_auction_system`.`user`
 'vamsi99',
 '9087206242',
 '285 George ST');
+
+CREATE TABLE `category` (
+  `category_id` int(2) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`category_id`)
+);
+
+CREATE TABLE `subcategory` (
+  `subcategory_id` int(2) NOT NULL AUTO_INCREMENT,
+  `subcategory_name` varchar(25) NOT NULL,
+  `category_id` int(2) NOT NULL,
+  PRIMARY KEY (`subcategory_id`),
+  FOREIGN KEY (`category_id`) REFERENCES category
+);
+
+CREATE TABLE `product` (
+  `product_id` int(2) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(25) NOT NULL,
+  `brand` varchar(25),
+  `colour` varchar(25),
+  `size` varchar(25),
+  `price` float(10) NOT NULL,
+  `subcategory_id` NOT NULL,
+  PRIMARY KEY (`product_id`),
+  FOREIGN KEY (`subcategory_id`) REFERENCES subcategory
+);
+
+CREATE TABLE `auction` (
+  `auction_id` int(5) NOT NULL AUTO_INCREMENT,
+  `email_id` varchar(45) NOT NULL,
+  `product_id` int(2) NOT NULL,
+  `end_time` TIMESTAMP, 
+  `start_time` TIMESTAMP, 
+  `increment_amount` float(10), 
+  `minimum_price` float(10), 
+  `initial_price` float(10), 
+  PRIMARY KEY (`auction_id`),
+  FOREIGN KEy (`email_id`) REFERENCES user,
+  FOREIGN KEY (`product_id`) REFERENCES product
+);
+
+Create Table `bid` (
+  `bid_id` int(10) NOT NULL AUTO_INCREMENT, 
+  `email_id` varchar(45) NOT NULL, 
+  `auction_id` int(5) NOT NULL,
+  `bidding_timestamp` TIMESTAMP, 
+  `amount` float(10),
+  PRIMARY KEY (`bid_id`), 
+  FOREIGN KEY (`email_id`) REFERENCES user (`email_id`), 
+  FOREIGN KEY (`auction_id`) REFERENCES `auction` (`auction_id`)
+);
+
+Create Table autobid (
+  `autobid_id` int(10) NOT NULL AUTO_INCREMENT, 
+  `email_id` varchar(45) NOT NULL, 
+  `auction_id` int(5) NOT NULL,
+  `increment` int(10) NOT NULL, 
+  `upper_limit` int(10) NOT NULL,
+  PRIMARY KEY (`autobid_id`), 
+  FOREIGN KEY (`email_id`) REFERENCES user (`email_id`), 
+  FOREIGN KEY (`auction_id`) REFERENCES `auction` (`auction_id`)
+);
+
+
 
 
 
---
+++ models/Product.js
@@ -0,0 +1,73 @@
+const db = require('../config/db');
+
+const Product = {
+    selectAllcategories: cb => {
+      const queryString =
+      'SELECT * FROM bm_auction_system.category;'
+      db.query(queryString, (err, results) => {
+        if (err) throw err
+        cb(results)
+      })
+    },
+    selectAllSubcategoriesByCategoryId: (id, cb) => {
+      const queryString =
+      'SELECT * from bm_auction_system.subcategory WHERE category_id=?;'
+      db.execute(queryString, [category_id], (err, results, fields) => {
+        if (err) throw err
+        cb(err, results)
+      })
+    },
+    selectAllProductsBySubcategoryId: (id, cb) => {
+        const queryString =
+        'SELECT product_id, product_name, brand, colour, size, price from bm_auction_system.product WHERE subcategory_id=?;'
+        db.execute(queryString, [subcategory_id], (err, results, fields) => {
+          if (err) throw err
+          cb(err, results)
+        })
+    },
+    filterProductsBySubcategoryId: (filters, cb) => {
+        const filter = "WHERE "
+        for (let key in filters) {
+            if (filters[key].length != 0) {
+                if (filter == 'WHERE ') {
+                    filter = filter.concat(" ", key, "= ", filters[key]);
+                }
+                else {
+                    filter = filter.concat("AND ", key, "= ", filters[key]);
+                }
+            }
+        }
+        const queryString =
+        'SELECT product_id, product_name, brand, colour, size, price from bm_auction_system.product ?'
+        db.execute(queryString, [filter], (err, results, fields) => {
+        if (err) throw err
+        cb(err, results)
+        })
+    },
+    selectProductByProductID: (username, cb) => {
+      const queryString =
+        'SELECT product_id, product_name, brand, colour, size, price FROM bm_auction_system.product WHERE product_id=?;'
+      db.execute(queryString, [product_id], (err, results, fields) => {
+        if (err) throw err
+        cb(results)
+      })
+    },
+    deleteOne: (id, cb) => {
+      const queryString = 'DELETE FROM product WHERE product_id=?;'
+      db.execute(queryString, [id], (err, result) => {
+        if (err) throw err
+        cb(result)
+      })
+    },
+    insertOne: (vals, cb) => {
+      const queryString =
+        'INSERT INTO `bm_auction_system`.`product`(`product_id`, `product_name`, `brand`, `colour`, `size`, `price`, `subcategory_id`) VALUES (?,?,?,?,?,?,?);'
+      db.execute(queryString, vals, (err, result) => {
+        console.log('result', result);
+        if (err) throw err
+        cb(err, result)
+      })
+    }
+  }
+
+module.exports = Product
