66dee96203d456e84a2f134a729d56e87258315f
Vamsi Krishna <bulusu.vk08@gmail.com>
2023-04-23 19:12:19 -0400

    changed redirect

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -34,7 +34,7 @@ const adminController = {
         try {
             const values = [req.body.email_id, await bcrypt.hash(req.body.password, 10), req.body.name, req.body.user_name, req.body.phone_number];
             await Admin.insertOneCR(values);
-            return res.redirect('homepage');
+            return res.redirect('/admin/homepage');
         } catch (err) {
             console.error(err);
             return res.status(500).json({ error: 'Internal server error' });
