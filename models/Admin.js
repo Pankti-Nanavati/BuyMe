a3ca651df7ad4abcea557f099174bb58bb22bd2b
darshee-m <darshee.m@somaiya.edu>
2023-05-01 00:02:57 -0400

    sales report

--- models/Admin.js
+++ models/Admin.js
@@ -56,17 +56,34 @@ const Admin = {
     }
   },
   getSalesReport: async (date1, date2) => {
-    const queryString1 = 'Select SUM(amount) from sales WHERE sale_timestamp <= ? and sale_timestamp >= ?;';
-    const queryString2 = 'Select buyer_email_id, SUM(amount) as user_earnings from sales WHERE sale_timestamp <= ? and sale_timestamp >= ? group by buyer_email_id order by user_earnings DESC limit 5;';
+    const queryString1 = 'Select SUM(amount) as total from sales WHERE sale_timestamp <= ? and sale_timestamp >= ?;';
+    const queryString2 = 'Select product_id, SUM(amount) as item_earnings from sales WHERE sale_timestamp <= ? and sale_timestamp >= ? group by buyer_email_id order by item_earnings DESC limit 5;';
     const queryString3 = 'Select buyer_email_id, SUM(amount) as user_earnings from sales WHERE sale_timestamp <= ? and sale_timestamp >= ? group by buyer_email_id order by user_earnings DESC limit 5;';
+    const queryString4 = 'Select product_id, SUM(amount) as item_earnings from sales WHERE sale_timestamp <= ? and sale_timestamp >= ? group by buyer_email_id order by item_earnings DESC';
+    const queryString5 = 'Select buyer_email_id, SUM(amount) as user_earnings from sales WHERE sale_timestamp <= ? and sale_timestamp >= ? group by buyer_email_id order by user_earnings DESC';
+    const queryString6 = 'Select P.subcategory_id, SUM(S.amount) as category_earnings from sales S inner join product P on P.product_id = S.product_id WHERE S.sale_timestamp <= ? and S.sale_timestamp >= ? group by P.subcategory_id order by category_earnings DESC';
     try{
       const [result1] = await db.execute(queryString1, [date2, date1]);
       const [result2] = await db.execute(queryString2, [date2, date1]);
       const [result3] = await db.execute(queryString3, [date2, date1]);
+      const [result4] = await db.execute(queryString4, [date2, date1]);
+      const [result5] = await db.execute(queryString5, [date2, date1]);
+      const [result6] = await db.execute(queryString6, [date2, date1]);
+      var topProducts = []
+      for(let i= 0; i< result2.length; i++) {
+        topProducts.push(result2[i].product_id);
+      }
+      var topUsers = []
+      for(let j= 0; j< result3.length; j++){
+        topUsers.push(result3[j].buyer_email_id);
+      }
       const result = {};
-      result['total_earnings'] = result1;
-      result['Top_products'] = result2;
-      result['Top_buyers'] = result3;
+      result['total_earnings'] = result1[0].total;
+      result['Top_products'] = topProducts;
+      result['Top_buyers'] = topUsers;
+      result['total_earnings_per_user'] = result4;
+      result['total_earnings_per_item'] = result5;
+      result['total_earnings_per_category'] = result6;
       return result;
     } catch (err) {
       console.error(err);
