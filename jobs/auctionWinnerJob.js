d2492ca95202592e69155a098cbb22bdf15b58f0
darshee-m <darshee.m@somaiya.edu>
2023-05-01 18:01:44 -0400

    if statements

--- jobs/auctionWinnerJob.js
+++ jobs/auctionWinnerJob.js
@@ -5,44 +5,46 @@ const runAuctionWinnerJob = async () => {
     try {
         console.log("calling function assignAuctionWinner");
         //fetching auctions that have ended and their winners have not been assigned
-        const endedAuctionsQuery = 'Select auction_id, minimum_price, email_id, product_id from `bm_auction_system`.`auction` where end_time < NOW() and has_winner = 0;';
+        const endedAuctionsQuery = 'Select A.auction_id, A.minimum_price, A.email_id, A.product_id P.product_name from `bm_auction_system`.`auction` A inner join `bm_auction_system`.`product` P on A.product_id = P.product_id where end_time < NOW() and has_winner = 0;';
         const [endedAuctions] = await db.execute(endedAuctionsQuery);
         console.log('Ended auctions', endedAuctions);
-        for(let i = 0; i < endedAuctions.length; i++){
-            const highestBidQuery = 'Select MAX(amount) as amount, email_id, bidding_timestamp from `bm_auction_system`.`bid` where auction_id = ? group by email_id, bidding_timestamp;';
-            const [highestBid] = await db.execute(highestBidQuery, [endedAuctions[i].auction_id]);
-            console.log('highestbid', highestBid);
-            console.log(highestBid.length > 0 && endedAuctions[i].minimum_price <= highestBid[0].amount);
-            if(highestBid.length != 0){
-                console.log('inside if');
-                if (highestBid.length > 0 && endedAuctions[i].minimum_price <= highestBid[0].amount){
-                    console.log('inside second if');
-                    const salesEntry = 'Insert into `bm_auction_system`.`sales` (buyer_email_id, seller_email_id, auction_id, product_id, amount, sale_timestamp) VALUES (?,?,?,?,?,?);';
-                    const [salesEntryRes] = await db.execute(salesEntry, [highestBid[0].email_id, endedAuctions[i].email_id, endedAuctions[i].auction_id, endedAuctions[i].product_id, highestBid[0].amount, highestBid[0].bidding_timestamp]);
-                    console.log('sales entry', salesEntryRes);
-                    var message = "";
-                    message = message.concat("Congratulations! You won the auction, the product ", product_name, "is for you to buy!");
-                    const prevMessageQuery = 'Select message from `bm_auction_system`.`notifications` where email_id = ? and seen = 0;';
-                    const [prevMessages] = await db.execute(prevMessageQuery, [highestBid[0].email_id]);
-                    var message_flag = 1
-                    if (prevMessages.length != 0){
-                        for(let m = 0; m < prevMessages.length; m++){
-                            if(message === prevMessages[m].message){
-                                message_flag = 0;
+        if(endedAuctions.length != 0){
+            for(let i = 0; i < endedAuctions.length; i++){
+                const highestBidQuery = 'Select MAX(amount) as amount, email_id, bidding_timestamp from `bm_auction_system`.`bid` where auction_id = ? group by email_id, bidding_timestamp;';
+                const [highestBid] = await db.execute(highestBidQuery, [endedAuctions[i].auction_id]);
+                console.log('highestbid', highestBid);
+                console.log(highestBid.length > 0 && endedAuctions[i].minimum_price <= highestBid[0].amount);
+                if(highestBid.length != 0){
+                    console.log('inside if');
+                    if (highestBid.length > 0 && endedAuctions[i].minimum_price <= highestBid[0].amount){
+                        console.log('inside second if');
+                        const salesEntry = 'Insert into `bm_auction_system`.`sales` (buyer_email_id, seller_email_id, auction_id, product_id, amount, sale_timestamp) VALUES (?,?,?,?,?,?);';
+                        const [salesEntryRes] = await db.execute(salesEntry, [highestBid[0].email_id, endedAuctions[i].email_id, endedAuctions[i].auction_id, endedAuctions[i].product_id, highestBid[0].amount, highestBid[0].bidding_timestamp]);
+                        console.log('sales entry', salesEntryRes);
+                        var message = "";
+                        message = message.concat("Congratulations! You won the auction, the product ", endedAuctions[i].product_name, "is for you to buy!");
+                        const prevMessageQuery = 'Select message from `bm_auction_system`.`notifications` where email_id = ? and seen = 0;';
+                        const [prevMessages] = await db.execute(prevMessageQuery, [highestBid[0].email_id]);
+                        var message_flag = 1
+                        if (prevMessages.length != 0){
+                            for(let m = 0; m < prevMessages.length; m++){
+                                if(message === prevMessages[m].message){
+                                    message_flag = 0;
+                                }
                             }
                         }
+                        if(message_flag){
+                            const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
+                            const [notifs] = await db.execute(notifQuery, [highestBid[0].email_id, message]);
+                            console.log('notifs', notifs);
+                        }
                     }
-                    if(message_flag){
-                        const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
-                        const [notifs] = await db.execute(notifQuery, [highestBid[0].email_id, message]);
-                        console.log('notifs', notifs);
-                    }
-                }
-                const updateAuction = 'Update `bm_auction_system`.`auction` set has_winner = 1 where auction_id = ?;';
-                const [updateAuctionRes] = await db.execute(updateAuction, [endedAuctions[i].auction_id]);
-                console.log('auction update query', updateAuctionRes);
-            }  
-        }    
+                    const updateAuction = 'Update `bm_auction_system`.`auction` set has_winner = 1 where auction_id = ?;';
+                    const [updateAuctionRes] = await db.execute(updateAuction, [endedAuctions[i].auction_id]);
+                    console.log('auction update query', updateAuctionRes);
+                }  
+            } 
+        }   
     }
     catch(err){
         console.log(err);
--- jobs/autobidJob.js
+++ jobs/autobidJob.js
@@ -5,66 +5,79 @@ const runAutoBid = async () => {
     try {
         console.log("Calling the place_autoBid function")
         //fetching all active auctions
-        const queryString = 'SELECT auction_id FROM bm_auction_system.auction where end_time > NOW();';
+        const queryString = 'SELECT auction_id, initial_price FROM bm_auction_system.auction where end_time > NOW();';
         const [rows] = await db.query(queryString);
-        for (let a = 0; a < rows.length; a++){
-            //fetching the highest bid
-            const currentBidQuery=  'select email_id, amount from bm_auction_system.bid where bidding_timestamp IN (select MAX(bidding_timestamp) from bid where auction_id = ?);'
-            const [bidRows] = await db.execute(currentBidQuery, [rows[a].auction_id]);
-            var currentBid = bidRows[0].amount;
-            const currentWinner = bidRows[0].email_id;
-            //fetching autobid users
-            const checkAutoBid = 'SELECT `email_id`, `increment`, `upper_limit` FROM `bm_auction_system`.`autobid` where auction_id = ?;';
-            const [autobids] = await db.execute(checkAutoBid, [rows[a].auction_id]);
-            for (let i = 0; i < autobids.length; i++) {
-                if(autobids[i].email_id != currentWinner){
-                    var new_bid = autobids[i].amount;
-                    new_bid = currentBid + autobids[i].increment;
-                    if (new_bid <= autobids[i].upper_limit){
-                        const query = 'INSERT INTO `bm_auction_system`.`bid` (`email_id`, `auction_id`, `bidding_timestamp`, `amount`) VALUES (?,?, NOW(), ?);';
-                        const [newbid] = await db.execute(query, [autobids[i].email_id, rows[a].auction_id, new_bid]);
-                        console.log("Autobid made a new bid", newbid);
-                        currentBid = new_bid;
-                    }
-                    else{
-                        //implement notification for upper limit here
-                        console.log("User's upper limit has reached");
-                        message = "The upper limit you have set has been reached, the system can not bid further";
-                        const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
-                        const [notifs] = await db.execute(notifQuery, [autobids[i].email_id, message]);
-                        console.log(notifs)
-                    }
+        if(rows.length != 0){
+            for (let a = 0; a < rows.length; a++){
+                //fetching the highest bid
+                const currentBidQuery=  'select email_id, amount from bm_auction_system.bid where bidding_timestamp IN (select MAX(bidding_timestamp) from bid where auction_id = ?);'
+                const [bidRows] = await db.execute(currentBidQuery, [rows[a].auction_id]);
+                var first_bidder = 0
+                if(bidRows.length != 0){
+                    var currentBid = bidRows[0].amount;
+                    var currentWinner = bidRows[0].email_id;
                 }
-            }
-            //fetching manual bidders
-            const checkManualBid = 'Select B.email_id from `bm_auction_system`.`autobid` A inner join `bm_auction_system`.`bid` B on A.email_id != B.email_id;';
-            const [manualbids] = await db.execute(checkManualBid, [rows[a].auction_id]);
-            for(let j = 0; j < manualbids.length; j++){
-                const currentUserBidQuery=  'select amount from bm_auction_system.bid where bidding_timestamp IN (select MAX(bidding_timestamp) from bid where email_id = ? and auction_id = ?);'
-                const [userBidRows] = await db.execute(currentUserBidQuery, [manualbids[j].email_id, rows[a].auction_id]);
-                const currentUserBid = userBidRows[0].amount;
-                if (currentUserBid < currentBid){
-                    const prevMessageQuery = 'Select message from `bm_auction_system`.`notifications` where email_id = ? and seen =0;';
-                    const [prevMessages] = await db.execute(prevMessageQuery, [manualbids[j].email_id]);
-                    var message_flag = 1
-                    message = "Someone exceeded your bid, the highest bid is now: $";
-                    message = message.concat(currentBid);
-                    if (prevMessages.length != 0){
-                        for(let m=0; m< prevMessages.length; m++){
-                            if(message === prevMessages[m].message){
-                                message_flag = 0;
+                else{
+                    var currentBid = rows[a].initial_price;
+                    var currentWinner = bidRows[0].email_id;
+                    first_bidder = 1;
+                }
+                //fetching autobid users
+                const checkAutoBid = 'SELECT `email_id`, `increment`, `upper_limit` FROM `bm_auction_system`.`autobid` where auction_id = ?;';
+                const [autobids] = await db.execute(checkAutoBid, [rows[a].auction_id]);
+                if(autobids.length != 0){
+                    for (let i = 0; i < autobids.length; i++) {
+                        if((autobids[i].email_id != currentWinner) || (first_bidder)){
+                            var new_bid = autobids[i].amount;
+                            new_bid = currentBid + autobids[i].increment;
+                            if (new_bid <= autobids[i].upper_limit){
+                                const query = 'INSERT INTO `bm_auction_system`.`bid` (`email_id`, `auction_id`, `bidding_timestamp`, `amount`) VALUES (?,?, NOW(), ?);';
+                                const [newbid] = await db.execute(query, [autobids[i].email_id, rows[a].auction_id, new_bid]);
+                                console.log("Autobid made a new bid", newbid);
+                                currentBid = new_bid;
+                            }
+                            else{
+                                //implement notification for upper limit here
+                                console.log("User's upper limit has reached");
+                                message = "The upper limit you have set has been reached, the system can not bid further";
+                                const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
+                                const [notifs] = await db.execute(notifQuery, [autobids[i].email_id, message]);
+                                console.log(notifs)
                             }
                         }
                     }
-                    if(message_flag){
-                        const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
-                        const [notifs] = await db.execute(notifQuery, [manualbids[j].email_id, message]);
-                        console.log(notifs)
+                }
+                //fetching manual bidders
+                const checkManualBid = 'Select B.email_id from `bm_auction_system`.`autobid` A inner join `bm_auction_system`.`bid` B on A.email_id != B.email_id;';
+                const [manualbids] = await db.execute(checkManualBid, [rows[a].auction_id]);
+                if(manualbids.length != 0){
+                    for(let j = 0; j < manualbids.length; j++){
+                        const currentUserBidQuery=  'select amount from bm_auction_system.bid where bidding_timestamp IN (select MAX(bidding_timestamp) from bid where email_id = ? and auction_id = ?);'
+                        const [userBidRows] = await db.execute(currentUserBidQuery, [manualbids[j].email_id, rows[a].auction_id]);
+                        const currentUserBid = userBidRows[0].amount;
+                        if (currentUserBid < currentBid){
+                            const prevMessageQuery = 'Select message from `bm_auction_system`.`notifications` where email_id = ? and seen =0;';
+                            const [prevMessages] = await db.execute(prevMessageQuery, [manualbids[j].email_id]);
+                            var message_flag = 1
+                            message = "Someone exceeded your bid, the highest bid is now: $";
+                            message = message.concat(currentBid);
+                            if (prevMessages.length != 0){
+                                for(let m=0; m< prevMessages.length; m++){
+                                    if(message === prevMessages[m].message){
+                                        message_flag = 0;
+                                    }
+                                }
+                            }
+                            if(message_flag){
+                                const notifQuery = 'Insert into `bm_auction_system`.`notifications` (email_id, message) VALUES (?,?);';
+                                const [notifs] = await db.execute(notifQuery, [manualbids[j].email_id, message]);
+                                console.log(notifs)
+                            }
+                        }
                     }
                 }
             }
         }
-    
     }
     catch(err){
         console.log(err);
